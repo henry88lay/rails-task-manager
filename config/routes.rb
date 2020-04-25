e2126f3715ae4d3a32cbf92368d6391bf7a76d96
henry lay <henry88lay@gmail.com>
2020-04-25 15:47:46 +1000

    completed the task

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
 
   # Update
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
   patch "tasks/:id", to: "tasks#update"
+
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy", as: :task_destroy
 
 end
